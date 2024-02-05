89f4b62887f97ccef1efce136d4248c46344ea54
Diogo Resende <dresende@thinkdigital.pt>
2024-02-05 11:04:39 +0000

    ci: adds more tests and improves code coverage

---
+++ test/Debug.js
@@ -0,0 +1,18 @@
+import { Renderer } from "../index.js"
+import should       from "should"
+
+const keikan = new Renderer({ debug: true });
+
+describe("Debug", () => {
+	it("outputs compiler in a more or less readable way", async () => {
+		const view = await keikan.compilePath(import.meta.dirname + "/views/has-include");
+
+		should(view.code.includes("\t")).be.true;
+	});
+
+	it("outputs view keeping all whitespace and use include indent", async () => {
+		const view = await keikan.compilePath(import.meta.dirname + "/views/has-include");
+
+		view({ name: "Diogo" }).should.equal("<h3>\n\tHello Diogo\n</h3>");
+	});
+});
--- test/Features.js
+++ test/Features.js
@@ -15,4 +15,11 @@ describe("Features", () => {
 
 		view().should.equal("<ul><li>This is a &quot;quoted&quot; string</li><li>This is not a \"quoted\" string</li><li></li></ul>");
 	});
+
+	it("supports code like conditions, loops, ..", async () => {
+		const view = await keikan.compilePath(import.meta.dirname + "/views/condition");
+
+		view({ condition: true }).should.equal("<h3>Hello\n</h3>");
+		view({ condition: false }).should.equal("<h3></h3>");
+	});
 });
