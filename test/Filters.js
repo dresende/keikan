0b8f49abfac0d378ba0245c9a2c78a85ebcaef2e
Diogo Resende <dresende@thinkdigital.pt>
2024-02-05 15:16:44 +0000

    ci: removes unnecessary html files and updates filters test coverage

--- test/Filters.js
+++ test/Filters.js
@@ -5,8 +5,14 @@ const keikan = new Renderer({ debug: true });
 
 describe("Filters", () => {
 	it("nl", async () => {
-		const view = await keikan.compilePath(import.meta.dirname + "/views/filter-nl");
+		const view = await keikan.compileData("<%-: \"text\\nwith\\nnewlines\" | nl %>");
 
 		view().should.equal("text<br>with<br>newlines");
+	});
+
+	it("quote", async () => {
+		const view = await keikan.compileData("<%-: \"<b>bold</b>\" | quote %>");
+
+		view().should.equal("&lt;b&gt;bold&lt;/b&gt;");
 	});
 });
