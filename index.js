b93a9d7c444996320debf37ab9f0e83a6bd6d4f0
Diogo Resende <dresende@thinkdigital.pt>
2024-02-05 17:28:37 +0000

    adds express support

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+import { Renderer } from "./core/Renderer.js";
+
 export { Renderer } from "./core/Renderer.js";
 export * as Filters from "./core/Filters.js";
+
+export const renderPath = async (filename, options, next) => {
+	// express checks file before passing to renderer, so view
+	// will never be null except there's an actual bug
+	const renderer = new Renderer();
+	const view     = await renderer.compilePath(filename);
+
+	return next(null, view(options));
+};
---
+++ test/Express.js
@@ -0,0 +1,48 @@
+import * as Keikan from "../index.js"
+import should      from "should"
+import http        from "http"
+import express     from "express"
+import { resolve } from "path";
+
+describe("Express", () => {
+	it("should expose a renderPath to render files to express", async () => {
+		const app    = express();
+		const server = http.createServer(app);
+
+		app.engine("html", Keikan.renderPath);
+		app.set("view engine", "html");
+		app.set("views", resolve(import.meta.dirname, "views"));
+
+		app.get("/", (req, res) => {
+			res.render("simple", { name: "Diogo" });
+		});
+
+		await server.listen(0);
+
+		const response = await request(`http://localhost:${server.address().port}/`);
+
+		response.should.equal("<h3>Hello Diogo</h3>");
+
+		await server.close();
+	});
+});
+
+const request = async (url) => {
+	return new Promise((resolve, reject) => {
+		const req = http.request(url, (res) => {
+			let buf = "";
+
+			res.on("data", (data) => {
+				buf += data;
+			});
+
+			res.on("end", () => {
+				return resolve(buf);
+			});
+		});
+
+		req.on("error", reject);
+
+		req.end();
+	});
+};
